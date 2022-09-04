5e1eb37f423100d04be571118596138991a8c283
Keane Dalisay <keane.dalisay0118@gmail.com>
2022-09-04 13:12:11 +0800

    Add individual objects for all buttons, dropdowns, and pages

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+const buttons = {
+    themeBtn : document.getElementById('theme-button'),
+    lightThemeBtn : document.getElementById('light-theme'),
+    darkThemeBtn : document.getElementById('dark-theme'),
+
+    pickFriendBtn : document.getElementById('pick-friend-button'),
+    pickAIBtn : document.getElementById('pick-ai-button'),
+
+    easyAIBtn : document.getElementById('ai-easy'),
+    mediumAIBtn : document.getElementById('ai-medium'),
+    hardAIBtn : document.getElementById('ai-hard'),
+
+    menuBtn : document.getElementById('menu-button'),
+    markXBtn : document.getElementById('pick-x-button'),
+    markOBtn : document.getElementById('pick-o-button'),
+};
+
+const dropdowns = {
+    themeDropdown : document.getElementById('theme-dropdown'),
+    modeAIDropdown : document.getElementById('ai-dropdown'),
+}
+
+const pages = {
+    splashPage : document.getElementById('splash-page'),
+    gamePage : document.getElementById('game-page'),
+}
